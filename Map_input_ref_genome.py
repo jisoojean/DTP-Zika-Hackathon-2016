1cdb4804f993e4766aff4105d401cd0d778ba885
jyouie <sheng.pong@lincoln.ox.ac.uk>
2016-11-30 10:51:53 +0000

    Update Map_input_ref_genome.py

--- Map_input_ref_genome.py
+++ Map_input_ref_genome.py
@@ -23,20 +23,28 @@ gencode = {
     'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
     'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'}
 
-g = open("%s_aa_seq.fasta" % input_file , "w")
+filename = ""
+for letter in input_file:
+	if letter == '.':
+		break
+	else:
+		filename += letter
+
+g = open("%s_aa_seq.fasta" % filename, "w")
 f = open(input_file, "r")
 
 content = [x.strip("\n") for x in f.readlines()]
-RNA_Genome =''
+genome_identity = ""
+RNA_Genome = ""
 for element in content:
 	if element.startswith(">"):
-		continue
+		genome_identity += element
 	else:
 		RNA_Genome += element
 
 
 start_codon = RNA_Genome.find("ATG")
-protein = ''
+protein = ""
 for nucleotide in range(start_codon, len(RNA_Genome), 3):
 	codon= RNA_Genome[nucleotide:nucleotide+3]
 	if len(codon) < 3:
@@ -47,16 +55,17 @@ for nucleotide in range(start_codon, len(RNA_Genome), 3):
 				break
 			else:
 				protein += amino_acid
-		
+
+g.write(genome_identity + "\n")
 g.write(protein)
 g.close()
 
 # Pairwise alignment by Needleman-Wunsch algorithm
-needle_cline = NeedleCommandline(asequence="zika_ref_aaseq.fasta", bsequence="%s_aa_seq.fasta" % input_file, gapopen=10, gapextend=0.5, outfile="%s_pwa.txt" % input_file)
+needle_cline = NeedleCommandline(asequence="zika_ref_aaseq.fasta", bsequence="%s_aa_seq.fasta" % filename, gapopen=10, gapextend=0.5, outfile="%s_pwa.txt" % filename)
 stdout, strderr = needle_cline()
 
 # Converting the alignment result into a manipulable format
-align = AlignIO.read("needle.txt", "emboss")
+align = AlignIO.read("%s_pwa.txt" % filename, "emboss")
 
 length_of_alignment = align.get_alignment_length()
 list_mutations = []
@@ -108,6 +117,3 @@ for insrt in list_insertions:
 			protein_names[k] = (v[0], v[1]+1)
 
 print protein_names
-
-
-
